19eab8b537fd66adbb5841ef1691696aeb24f2b0
Samridhi Agarwal <samridhi.agarwal@cumminscollege.in>
2021-10-23 12:01:07 +0530

    anagram code in c

---
+++ angram1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int i=0,c=0,d=0,e,f;
+	char a[100],b[100];
+	printf("enter two words to check anagram : ");
+    scanf("%s %s",a,b);
+	}
+    for(i=0;i<c;i++){
+    	while(b[e]==a[i]){
+    		e=e+1;
+		}
+		printf("it is");
+	}
+    return 1;
+}
